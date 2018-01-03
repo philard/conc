83d47fbded48eadbec52f98fd8c8a7831db7250a
Philip John Ardley <Philip_John_Ardley@epam.com>
2018-01-03 11:49:55 +0800

    chapter 3's loadTheArk
    Todo actually run this thread safe method

--- src/main/java/phil/conc/ch3/ArkLoader.java
+++ src/main/java/phil/conc/ch3/ArkLoader.java
@@ -1,4 +1,41 @@
 package phil.conc.ch3;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
 public class ArkLoader {
+
+    private ArrayList<AnimalPair> ark;
+
+    public int loadTheArk(Collection<Animal> candidates) {
+        HashSet<Animal> animals;
+        int numPairs = 0;
+        Animal candidate = null;
+// animals confined to method, don’t let them escape!
+        animals = new HashSet<Animal>();
+        animals.addAll(candidates);
+        for (Animal a : animals) {
+            if (candidate == null || !candidate.isPotentialMate(a))
+                candidate = a;
+            else {
+                ark.add(new AnimalPair(candidate, a));
+                ++numPairs;
+                candidate = null;
+            }
+        }
+        return numPairs;
+    }
+
+    private class Animal {
+        public boolean isPotentialMate(Animal a) {
+            return true;
+        }
+    }
+
+    private class AnimalPair {
+        public AnimalPair(Animal candidate, Animal a) {
+            super();
+        }
+    }
 }
